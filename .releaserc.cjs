3651841aec3bc8480e35aea3878745bf03552318
Justin Kimbrell <jkimbrell@actengag.com>
2023-10-11 16:06:12 -0600

    fix: more updates to the release tools

--- .releaserc.cjs
+++ .releaserc.cjs
@@ -12,12 +12,7 @@ module.exports = {
     plugins: [
         '@semantic-release/commit-analyzer',
         '@semantic-release/release-notes-generator',
-        [
-          "@semantic-release/npm",
-          {
-            "npmPublish": false
-          }
-        ],
+        "@semantic-release/npm",
         '@semantic-release/changelog',
         [
             '@semantic-release/git',
